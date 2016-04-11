844f4a027fa97d9396525d5465465fb28f733655
Chase Geigle <sky@skystrife.com>
2016-04-10 23:22:19 -0500

    libfolia 1.0.1

--- libfolia.rb
+++ libfolia.rb
@@ -1,9 +1,8 @@
 class Libfolia < Formula
   desc "XML annotation format for linguistically annotated language resources"
   homepage "https://proycon.github.io/folia/"
-  url "http://software.ticc.uvt.nl/libfolia-0.13.tar.gz"
-  sha256 "a9fc9e475bb79629dc014cf7a78af64486c0f1bb902821ba2bb36c38c77a5d24"
-  revision 1
+  url "https://github.com/LanguageMachines/libfolia/releases/download/v1.0.1/libfolia-1.0.1.tar.gz"
+  sha256 "ce95df49c656932d14cec8329a3ee5ad9b114d09962161fd9f1f368a4f90d8ab"
 
   bottle do
     cellar :any
